828e2767513d424f1690ab4bbb8b1f0b1d25fa96
陆文斌 <luwenbin@utourworld.com>
2017-07-27 16:47:03 +0800

    更新js

--- pages/forme/forme.js
+++ pages/forme/forme.js
@@ -13,6 +13,7 @@ var showModel = (title, content) => {
   });
 };
 var data_list='';
+var isload = true;
 
 Page({
   data: {
@@ -49,48 +50,76 @@ Page({
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReady: function () {
+    var userInfo = '';
     var _this = this;
-    var page = _this.data.page;
-    var index = _this.data.index;
-    var new_page = (parseInt(page)+1)*10;
-    console.log(new_page);
-    wx.request({
-      url: config.service.formeUrl,
-      data: {
-        page: new_page,
-        index: index
-      },
-      header: {
-        'content-type': 'application/json'
-      },
+    wx.getUserInfo({
       success: function (res) {
+        userInfo = res.userInfo;
         _this.setData({
-          "page": parseInt(page) + 1
+          "user": userInfo.nickName
         });
-        if (res.statusCode == 200) {
-          var data_list = res.data;
-          if (data_list.length === 0){
-            wx.showToast({
-              title: "到底啦！",
-              icon: 'success'
-            });
-          }
-          var data_list_old = _this.data.list;
-          for (var i = 0; i < data_list.length;i++){
-            data_list_old.push(data_list[i]);
-          }
-          _this.setData({
-            "list": data_list_old
-          });
-        } else {
-          showModel('网络出错', '请联系管理员');
-        }
-      },
-      error: function () {
-        showModel('网络出错', '请联系管理员');
       }
     });
+  },
+  del(event) {
+    var id = event.currentTarget.dataset.id;
+    var list = this.data.list;
+    for (var i = 0; i < list.length; i++){
+      if (list[i].id === id){
+        list.splice(i, 1);
+      }
+    }
+    this.setData({
+      "list": list
+    });
+  },
+
+  onReachBottom: function () {
+    if(isload){
+      var _this = this;
+      var page = _this.data.page;
+      var index = _this.data.index;
+      var new_page = (parseInt(page)+1)*10;
+      console.log(new_page);
+      wx.request({
+        url: config.service.formeUrl,
+        data: {
+          page: new_page,
+          index: index
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (res) {
+          _this.setData({
+            "page": parseInt(page) + 1
+          });
+          if (res.statusCode == 200) {
+            var data_list = res.data;
+            if (data_list.length === 0){
+              wx.showToast({
+                title: "到底啦！",
+                icon: 'success'
+              });
+              isload = false;
+            }
+            var data_list_old = _this.data.list;
+            for (var i = 0; i < data_list.length;i++){
+              data_list_old.push(data_list[i]);
+            }
+            _this.setData({
+              "list": data_list_old
+            });
+          } else {
+            showModel('网络出错', '请联系管理员');
+          }
+        },
+        error: function () {
+          showModel('网络出错', '请联系管理员');
+        }
+      });
+    }
   }
 })
 
