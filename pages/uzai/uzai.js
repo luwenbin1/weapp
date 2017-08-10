7018a0a32b51e923a8f5e2bb4fc4de94ff298f69
陆文斌 <luwenbin@utourworld.com>
2017-08-10 14:33:12 +0800

    增加loading

--- pages/uzai/uzai.js
+++ pages/uzai/uzai.js
@@ -6,13 +6,13 @@ Page({
    */
   data: {
     message: '',
-    on_class:[1,0,0,0]
+    on_class: [1, 0, 0, 0]
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    list('5CE3521C-6531-492E-AE71-E240E25DF6E9',this);
+    list('5CE3521C-6531-492E-AE71-E240E25DF6E9', this);
   },
 
   /**
@@ -26,44 +26,44 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
-  tapName:function(e){
+  tapName: function (e) {
     list(e.target.dataset.positionid, this);
     var index = e.target.dataset.index;
     var arr = [0, 0, 0, 0];
@@ -75,14 +75,20 @@ Page({
     console.log()
   }
 })
-function list(v, _this){
+function list(v, _this) {
+  wx.showToast({
+    title: '正在加载！',
+    icon: 'loading',
+    duration: 10000
+  });
   wx.request({
-    url: 'https://m.uzai.com/Index/Product?PositionId='+ v +'&Cityid=1', //仅为示例，并非真实的接口地址
+    url: 'https://m.uzai.com/Index/Product?PositionId=' + v + '&Cityid=1', //仅为示例，并非真实的接口地址
     data: {},
     header: {
       'content-type': 'text/html'
     },
     success: function (res) {
+      wx.hideToast();
       console.log(res);
       var data = res.data;
       data = data.replace(/data-src/g, "src")
